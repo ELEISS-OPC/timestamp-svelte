46cab08db931c55dc1b2aef026625f678bdbf196
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-03-18 14:44:48 +0800

    ⚙️ setup (svelte.config.js): use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
-	}
+  kit: {
+    adapter: adapter({
+      images: {
+        sizes: [640, 828, 1200, 1920, 3840],
+        formats: ["image/avif", "image/webp"],
+        minimumCacheTTL: 300,
+        domains: ["timestamp-svelte.vercel.app"],
+      },
+    }),
+  },
+  vitePlugin: {
+    dynamicCompileOptions: ({ filename }) =>
+      filename.includes("node_modules") ? undefined : { runes: true },
+  },
 };
 
 export default config;
