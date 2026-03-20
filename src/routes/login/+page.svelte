2e8b0cd121528737a3569ed0128e0beabdd51acc
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-03-20 09:31:58 +0800

    📦 new (page): adding login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { PageData } from "./$types.js";
+  import LoginForm from "./login-form.svelte";
+  let { data }: { data: PageData } = $props();
+</script>
+
+<div
+  class="w-screen h-screen flex flex-col items-center justify-center bg-orange-50"
+>
+  <h1 class="text-3xl font-bold mb-4">Login</h1>
+  <div
+    class="flex justify-center bg-white shadow-md rounded-md p-4 w-xs max-w-xs z-1"
+  >
+    <LoginForm {data} />
+  </div>
+</div>
