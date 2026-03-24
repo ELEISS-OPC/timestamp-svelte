179e2ccfff42d6ebbe8adb7529ef68dffa0a3b65
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-03-25 07:10:29 +0800

    🔧 update (summary page): fix to correct naming scheme and values

--- src/routes/dashboard/summary/+page.svelte
+++ src/routes/dashboard/summary/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import Histogram from "./histogram.svelte";
   import Metrics from "./metrics.svelte";
+  import type { PageProps } from "./$types";
+
+  let { data }: PageProps = $props();
 </script>
 
 <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-  <Histogram />
+  <Histogram chartData={data.chartData} />
   <Metrics />
 </div>
