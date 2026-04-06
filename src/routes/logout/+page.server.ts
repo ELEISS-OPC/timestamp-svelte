3ed6e2fddaaa8632bf49fd475b02b14dec53e2e8
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-04-06 11:42:55 +0800

    📦 new: implement logout functionality with cookie deletion and redirect

---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,25 @@
+import type { PageServerLoad } from "./$types";
+import { redirect } from "@sveltejs/kit";
+
+const deleteCookie = (cookies: any, name: string) => {
+  cookies.delete(name, { path: "/" });
+};
+
+const userCookieFields = [
+  "id",
+  "first_name",
+  "middle_name",
+  "last_name",
+  "email",
+  "role_id",
+  "avatar_url",
+  "avatar_url_preview",
+];
+
+export const load = (async ({ cookies }) => {
+  deleteCookie(cookies, "auth_token");
+  userCookieFields.forEach((field) => {
+    deleteCookie(cookies, `user_info-${field}`);
+  });
+  throw redirect(303, "/login");
+}) satisfies PageServerLoad;
