6d29f09df45774e37c0d49ce55cf8a228f07cb69
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-04-06 11:09:10 +0800

    📦 new (utils): add urlJoin function for joining URL parts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,12 +2,29 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
+export type WithoutChildren<T> = T extends { children?: any }
+  ? Omit<T, "children">
+  : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
-export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
+  ref?: U | null;
+};
+
+export function urlJoin(...parts: string[]) {
+  return parts
+    .map((part, index) => {
+      if (index === 0) {
+        return part.replace(/\/+$/g, ""); // Remove trailing slashes from the first part
+      } else {
+        return part.replace(/^\/+|\/+$/g, ""); // Remove leading and trailing slashes from other parts
+      }
+    })
+    .filter((part) => part.length > 0) // Remove empty parts
+    .join("/");
+}
