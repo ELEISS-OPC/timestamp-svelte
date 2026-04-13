60c43aaa87dc31a0619776594e67902b360b3e78
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-04-13 21:21:47 +0800

    🔧 update: refactor time display in attendance data tables to include timezone handling

--- src/routes/dashboard/attendance/data-table-time-in.svelte
+++ src/routes/dashboard/attendance/data-table-time-in.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+  import { PUBLIC_TIMEZONE } from "$env/static/public";
   import type { Row } from "@tanstack/table-core";
   import type { Schema } from "./schemas";
 
   let { row }: { row: Row<Schema> } = $props();
 </script>
 
-{new Date(row.original.time_out).toLocaleTimeString("en-US", {
+{new Date(row.original.time_in).toLocaleTimeString("en-US", {
   hour: "2-digit",
   minute: "2-digit",
+  timeZone: PUBLIC_TIMEZONE,
 })}
--- src/routes/dashboard/attendance/data-table-time-out.svelte
+++ src/routes/dashboard/attendance/data-table-time-out.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+  import { PUBLIC_TIMEZONE } from "$env/static/public";
   import type { Row } from "@tanstack/table-core";
   import type { Schema } from "./schemas";
 
   let { row }: { row: Row<Schema> } = $props();
 </script>
 
-{new Date(row.original.time_out).toLocaleTimeString("en-US", {
-  hour: "2-digit",
-  minute: "2-digit",
-})}
+{#if row.original.time_out}
+  {new Date(row.original.time_out).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: PUBLIC_TIMEZONE,
+  })}
+{/if}
