89c92b37f8823f94bcada18f4ecdc4d001882069
Chris Irag <iragca@pm.me>
2026-04-13 14:40:57 +0800

    🔧 update: modify attendance status display to use TimestampStatus enum

--- src/routes/dashboard/attendance/data-table-status.svelte
+++ src/routes/dashboard/attendance/data-table-status.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { Badge } from "$lib/components/ui/badge/index.js";
 	import CircleCheckFilledIcon from "@tabler/icons-svelte/icons/circle-check-filled";
-	import LoaderIcon from "@tabler/icons-svelte/icons/loader";
+	import CircleIcon from "@tabler/icons-svelte/icons/circle";
 	import type { Row } from "@tanstack/table-core";
 	import type { Schema } from "./schemas";
+	import { TimestampStatus } from "$lib/enums";
 
 	let { row }: { row: Row<Schema> } = $props();
 </script>
 
 <Badge variant="outline" class="text-muted-foreground px-1.5">
-	{#if row.original.status === "Done"}
+	{#if row.original.status == TimestampStatus.TIMED_IN}
 		<CircleCheckFilledIcon class="fill-green-500 dark:fill-green-400" />
+		Timed in
 	{:else}
-		<LoaderIcon />
+		<CircleIcon/>
+		Timed out
 	{/if}
-	{row.original.status}
 </Badge>
