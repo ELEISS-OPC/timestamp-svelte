39c06309951ee0da23fa8f80c5aceafd4b3a1396
Chris Irag <iragca@pm.me>
2026-04-13 15:02:21 +0800

    📦 new: create data table component for displaying employee attendance details

---
+++ src/routes/dashboard/attendance/data-table-employee.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Row } from "@tanstack/table-core";
+  import type { Schema } from "./schemas";
+  import type { EmployeeSchema } from "./schemas";
+
+  let { row }: { row: Row<Schema> } = $props();
+  const employee: EmployeeSchema = row.original.employee;
+</script>
+
+{employee.first_name}
+{employee.middle_name}
+{employee.last_name}
