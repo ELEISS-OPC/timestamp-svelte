935ddb6ef484310daf21fc6f606c52d882432eaf
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-04-15 10:11:20 +0800

    🔧 update: correct user email and password values in initial data insert

--- .init/1-inserts.sql
+++ .init/1-inserts.sql
@@ -8,6 +8,6 @@ INSERT INTO role (id, name) VALUES
 
 
 INSERT INTO users (id, first_name, middle_name, last_name, email, password, role_id) VALUES
-(1, 'John', 'Allister', 'Doe', 'john.doe@example.com', 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', 3),
-(2, 'Jane', 'Marie', 'Smith', 'jane.smith@example.com', 'c6ba91b90d922e159893f46c387e5dc1b3dc5c101a5a4522f03b987177a24a91', 2),
-(3, 'Alice', 'Grace', 'Johnson', 'alice.johnson@example.com', '5efc2b017da4f7736d192a74dde5891369e0685d4d38f2a455b6fcdab282df9c', 1);
+(1, 'John', 'Allister', 'Doe', 'admin.dev@infinetsolutionsph.com', 'e4abae53cc1cebe5fe89ea93882c699a5e71ab0bbf42a83b7d833975b61c4a41', 3),
+(2, 'Jane', 'Marie', 'Smith', 'officer.dev@infinetsolutionsph.com', 'eab0ecabe9f7bd1305bf7d1143a7c8ee33d85d1bb06649e5e498db012f6524f6', 2),
+(3, 'Alice', 'Grace', 'Johnson', 'employee.dev@infinetsolutionsph.com', '614d83fb28660621a01845a6c67c853b401d3bee31974d03433db829b30f5c2d', 1);
