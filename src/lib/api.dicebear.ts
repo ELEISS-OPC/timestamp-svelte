9b725520ec98e848918cc878b97874a0ed8c5d78
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2026-04-16 18:37:49 +0800

    🔧 update: change default avatar style in getAvatarUrl function to "fun-emoji" and update API version

--- src/lib/api.dicebear.ts
+++ src/lib/api.dicebear.ts
@@ -2,10 +2,10 @@ import type { Employee, dicebearOptions } from "$lib/types";
 
 export async function getAvatarUrl(
   seed: string,
-  options: dicebearOptions = { scale: 80, style: "initials" },
+  options: dicebearOptions = { scale: 80, style: "fun-emoji" },
 ): Promise<string> {
   const { scale = 80, style = "initials" } = options;
-  return `https://api.dicebear.com/6.x/${style}/png?seed=${seed}&scale=${scale}`;
+  return `https://api.dicebear.com/9.x/${style}/png?seed=${seed}&scale=${scale}`;
 }
 
 function formatEmployeeToSeed(
