a829ad849d0eb87f3da2756f1a8b5603d121b747
Chris Irag <iragca@pm.me>
2026-04-21 09:25:18 +0800

    🔧 update (login/schema.ts): enhance password validation messages for clarity

--- src/routes/login/schema.ts
+++ src/routes/login/schema.ts
@@ -2,7 +2,10 @@ import { z } from "zod";
 
 export const formSchema = z.object({
   email: z.email(),
-  password: z.string().min(8).max(100),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(100, "Password must be less than 100 characters long"),
 });
 
 export type FormSchema = typeof formSchema;
